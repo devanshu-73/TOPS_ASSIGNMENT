23036418c95e2ee165ff00ffbf6b297f319e1568
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-02-05 17:41:25 +0530

    updated Module- 3

---
+++ Module-3(Fundamentals of Programming)/Module-3.1(Que-1).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main ()
+{
+     printf("My name is Devanshu Chauhan.\n\n");
+     printf("My birth date is 07/03/2000.\n\n");
+     printf("I am 22 years old.\n\n");
+     printf("Ahmedabad Gujarat.\n\n");
+}
