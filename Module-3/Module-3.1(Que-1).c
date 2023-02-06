9d9a88b95b4f8b7c929dfb8afbc250d875abd1bb
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-02-06 15:17:47 +0530

    Updated Module-3

--- Module-3/Module-3.1(Que-1).c
+++ Module-3/Module-3.1(Que-1).c
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-#include<stdio.h>
+#include <stdio.h>
 
-void main ()
+void main()
 {
      printf("My name is Devanshu Chauhan.\n\n");
      printf("My birth date is 07/03/2000.\n\n");
      printf("I am 22 years old.\n\n");
      printf("Ahmedabad Gujarat.\n\n");
-=======
-#include<stdio.h>
-
-void main ()
-{
-     printf("My name is Devanshu Chauhan.\n\n");
-     printf("My birth date is 07/03/2000.\n\n");
-     printf("I am 22 years old.\n\n");
-     printf("Ahmedabad Gujarat.\n\n");
->>>>>>> 9b7024ac382f96a6e36ead29a0c5c3a9cc1beeab
 }
