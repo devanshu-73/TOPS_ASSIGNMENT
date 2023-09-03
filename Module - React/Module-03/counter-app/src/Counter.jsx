df59076b941328d13dff3d817ba1fa4abd738e86
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-03 19:14:09 +0530

    ...

--- Module - React/Module-03/counter-app/src/Counter.jsx
+++ Module - React/Module-03/counter-app/src/Counter.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import './Counter.css';
 const Counter = () => {
     const [count, setCount] = useState(0);
 
     return (
         <>
+                <h1 id="title"> React Web</h1>
             <div>
-                <button onClick={ () => setCount(count => count - 1)} style={{ marginRight: 10 }}> - </button>
+                <button id="minus" onClick={() => setCount(count => count - 1)} style={{ marginRight: 10 }}> - </button>
                 <span id="center">{count}</span>
-                <button onClick={ () => setCount(count => count + 1)} style={{ marginLeft: 10 }}> + </button>
+                <button id="plus" onClick={() => setCount(count => count + 1)} style={{ marginLeft: 10 }}> + </button>
             </div>
         </>
     )
