1bd7198e4a260dc92861ffb24fb1db8d3bff4b9a
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-05 17:23:43 +0530

    Python Assignment...

---
+++ Python Assignment/Module - 02 (Fundamentals of python)/Q-01.py
@@ -0,0 +1,8 @@
+# Check if a number is positive, negative, or zero
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
