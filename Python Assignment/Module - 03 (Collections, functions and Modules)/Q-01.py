cc8deaa965146e4e5f67c68db844433ee37255d2
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-18 17:24:47 +0530

    Assignment

--- Python Assignment/Module - 03 (Collections, functions and Modules)/Q-01.py
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-01.py
@@ -1,2 +1,10 @@
 #  What is List? How will you reverse a list? 
 
+'''
+  1) Lists are used to store multiple items in a single variable.
+  2) Lists are one of 4 built-in data types in Python used to store collections of data, 
+      the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+'''
+list1 = [1,2,3,4,5,6,7,8]
+
+print("Reversed List:",list1[::-1])
