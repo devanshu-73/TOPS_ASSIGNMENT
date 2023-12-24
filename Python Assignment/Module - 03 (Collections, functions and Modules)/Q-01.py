d8313ae4823ec820b3feaf08d94f55f413787e55
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-24 22:11:24 +0530

    ....

--- Python Assignment/Module - 03 (Collections, functions and Modules)/Q-01.py
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-01.py
@@ -2,9 +2,49 @@
 
 '''
   1) Lists are used to store multiple items in a single variable.
-  2) Lists are one of 4 built-in data types in Python used to store collections of data, 
-      the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+  2) Lists are one of 4 built-in data types in Python used to store collections of data,
+     lists are mutable,ordered collection of elements
 '''
 list1 = [1,2,3,4,5,6,7,8]
 
+# way 1 using Slicing
 print("Reversed List:",list1[::-1])
+
+# way 2 using inbuilt fun reverse() 
+list1.reverse()
+print("Reversed list:", list1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
