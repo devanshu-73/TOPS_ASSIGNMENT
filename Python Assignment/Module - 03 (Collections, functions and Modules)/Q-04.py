b7a204850c4664d63ac06df2b96f2de157ca7f61
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-24 23:34:15 +0530

    Assignment

--- Python Assignment/Module - 03 (Collections, functions and Modules)/Q-04.py
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-04.py
@@ -1,24 +1,32 @@
 #  Write a Python function to get the largest number, smallest num and sum of all from a list.
 
-def func1(numbers):
-    if not numbers:
-        return None, None, 0  # Return None for largest and smallest if the list is empty
-
-    largest = smallest = numbers[0]
+def func1(list1):
+    if not list1:
+        return None, None, None  # Return a tuple of None for an empty list
+    
+    largest = smallest = list1[0]
     total_sum = 0
-
-    for num in numbers:
+    
+    for num in list1:
         total_sum += num
+
+        # Find largest num
         if num > largest:
             largest = num
+
+        # Find smallest num
         elif num < smallest:
             smallest = num
 
     return largest, smallest, total_sum
+    
 
-numbers_list = [5, 2, 8, 1, 7]
-largest_num, smallest_num, sum_all = func1(numbers_list)
+list1 = [1,2,3]
+result = func1(list1)
 
-print("Largest Number:", largest_num)
-print("Smallest Number:", smallest_num)
-print("Sum of All Numbers:", sum_all)
+if result:  # Check if the result is not None before accessing elements
+    print("Largest Number:", result[0])
+    print("Smallest Number:", result[1])
+    print("Sum of All Numbers:", result[2])
+else:
+    print("Empty list...")
