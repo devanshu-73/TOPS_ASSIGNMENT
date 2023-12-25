44371f3f128c674ba41c84e7dff0073157fa7c6b
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-26 00:22:47 +0530

    Assignment

--- Python Assignment/Module - 03 (Collections, functions and Modules)/Q-16.py
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-16.py
@@ -1,20 +1,11 @@
 # check whether a list contains a sub list
 
-main_list = [1, 2, 3, 4, 5, 6, 7]
-sub_list = [3, 4]
+main_list = [1, 2, 3, 4, 5, 6, 7, 8]
+sublist = [3, 4, 5]
 
-contains_sublist = False
-for i in range(len(main_list) - len(sub_list) + 1):
-    match = True
-    for j in range(len(sub_list)):
-        if main_list[i + j] != sub_list[j]:
-            match = False
-            break
-    if match:
-        contains_sublist = True
+for item in sublist:
+    if item not in main_list:
+        print("list does not contain the sublist.")
         break
-
-if contains_sublist:
-    print("Main list contains the sub-list.")
 else:
-    print("Main list does not contain the sub-list.")
+    print("list contains the sublist.")
---
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-17.py
@@ -0,0 +1,7 @@
+# split list into different variables
+
+list_1 = [1, 2, 3]
+var1, var2, var3 = list_1
+print("Variable 1 :",var1)
+print("Variable 2 :",var2)
+print("Variable 3 :",var3)
