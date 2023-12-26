3bf067b92b0719d1aab329cdcc8b22999eecab32
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-26 20:09:01 +0530

    Assignment

--- Python Assignment/Module - 03 (Collections, functions and Modules)/Q-32.py
+++ Python Assignment/Module - 03 (Collections, functions and Modules)/Q-32.py
@@ -1,24 +1,12 @@
 
 my_dict = {'a': 50, 'b': 30, 'c': 80, 'd': 10}
 
-sorted_items_asc = list(my_dict.items())
-sorted_items_asc.sort(key=lambda x: x[1]) # x[1] means getting value of dict
+asc_list = list(my_dict.items())
+asc_list.sort(key=lambda x: x[1]) # x[1] means getting value of dict
 
-sorted_items_desc = list(my_dict.items())
-sorted_items_desc.sort(key=lambda x: x[1], reverse=True)
+desc_list = list(my_dict.items())
+desc_list.sort(key=lambda x: x[1], reverse=True)
 
-print("Ascending Order:",dict(sorted_items_asc))
-print("\nDescending Order:",dict(sorted_items_desc))
+print("Ascending Order:",dict(asc_list))
+print("Descending Order:",dict(desc_list))
 
-
-
-# my_dict = {'a': 50, 'b': 30, 'c': 80, 'd': 10}
-
-# # ascending order
-# sorted_dict_asc = dict(sorted(my_dict.items(), key=lambda item: item[1]))
-
-# # descending order
-# sorted_dict_desc = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
-
-# print("Ascending Order:", sorted_dict_asc)
-# print("Descending Order:", sorted_dict_desc)
