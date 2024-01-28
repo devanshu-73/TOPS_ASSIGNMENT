f5d620346be73d5b9a03f198e12a6144b6591694
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-28 20:21:45 +0530

    Assessment

--- Python Assignment/Module – 06 (Rest Framework)/myproject/myapp/models.py
+++ Python Assignment/Module – 06 (Rest Framework)/myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    isbn = models.CharField(max_length=13)
+    publisher = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
--- Python Assignment/Module – 06 (Rest Framework)/myproject/myapp/views.py
+++ Python Assignment/Module – 06 (Rest Framework)/myproject/myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    print("hello")
+    return HttpResponse("Hello")
